5e3a1277a3e98df0b8c8a4dcfbc31d43428d887f
KnabeBen <ebank@Bens-Air.fritz.box>
2016-06-19 14:48:03 +0200

    empty line

--- main.cpp
+++ main.cpp
@@ -18,7 +18,6 @@ using namespace std;
 int main(int argc, char** argv) {
     int i = 0;
     allocate ersterZerstoerer;
-
 // Test Kommentar
     ersterZerstoerer.allcocate(2, 1000000); // eingabe in 1000
 // 1== chrash   2== fast-chrash  bsp.: 1000000*1000 = 1G Durchläufe belegt 
